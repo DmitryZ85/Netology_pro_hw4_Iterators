68016f5b93841a7649494a51283f113cfeae10c1
Dmitry Z <zmamohtz@gmail.com>
2023-09-06 00:58:58 +0300

    Iterators, Generators first commit

---
+++ Iterator.py
@@ -0,0 +1,41 @@
+# Итератор
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list_upd = sum(list_of_list, [])
+
+    def __iter__(self):
+        self.counter = -1
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        if self.counter == len(self.list_of_list_upd):
+            raise StopIteration
+        item = self.list_of_list_upd[self.counter]
+        return item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    print(list(FlatIterator(list_of_lists_1)))
+
+if __name__ == '__main__':
+    test_1()
+
+
+
